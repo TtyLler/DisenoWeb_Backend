b8d8eabffd49449ecec3a4e60faa0fc01a5408a5
Tyler Thompson <tylertho2001@hotmail.com>
2023-07-07 17:41:18 -0600

    add CRUD functionalities to Restaurante Controller

--- src/controllers/restaurante.js
+++ src/controllers/restaurante.js
@@ -23,4 +23,46 @@ export const getOneRestaurant = async (req, res) => {
   }
 }
 
-//TODO:INCLUIR EL RESTO DE METODOS PARA EL CRUD DE RESTAURANTE
+export const createRestaurant = async (req, res) => {
+  const data = new Restaurant({
+      CodigoRestaurante: req.body.CodigoRestaurante,
+      NombreRestaurante: req.body.NombreRestaurante,
+      DireccionRestaurante: req.body.apellDireccionRestauranteidos,
+      CantidadClientes: req.body.CantidadClientes,
+      Telefono: req.body.Telefono
+  })
+  try {
+      const dataToSave = await data.save();
+      res.status(200).json(dataToSave)
+  }
+  catch (error) {
+      res.status(400).json({message: error.message})
+  }
+}
+
+export const updateRestaurant = async (req, res) => {
+  try {
+      const { id } = req.params;
+      const updatedData = req.body;
+      const options = { new: true };
+
+      const result = await Restaurant.findByIdAndUpdate(
+          id, updatedData, options
+      )
+      res.send(result)
+  }
+  catch (error) {
+      res.status(400).json({ message: error.message })
+  }
+}
+
+export const deleteRestaurant = async (req, res) => {
+  try {
+      const { id } = req.params;
+      const data = await Restaurant.findByIdAndDelete(id)
+      res.send(`Restaurant "${data.NombreRestaurante}" has been deleted`)
+  }
+  catch (error) {
+      res.status(400).json({ message: error.message })
+  }
+}
--- src/routes/restaurante.js
+++ src/routes/restaurante.js
@@ -1,9 +1,12 @@
 import { Router} from "express";
-import { getRestaurant, getOneRestaurant } from '../controllers/restaurante.js'
+import { getRestaurant, getOneRestaurant, createRestaurant, updateRestaurant, deleteRestaurant } from '../controllers/restaurante.js'
 
 const router = Router();
 
 router.get('/restaurants', getRestaurant)
 router.get('/restaurants/:id', getOneRestaurant)
+router.post('/restaurants', createRestaurant)
+router.put('/restaurants/:id',updateRestaurant)
+router.delete('/restaurants/:id',deleteRestaurant)
 
 export default router;
